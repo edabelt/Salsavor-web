afb0be257c74b6e1223514510d9d33d4a87629ba
David Beltran <davidbeltran@Davids-MacBook-Pro.local>
2025-03-19 18:20:59 -0500

    Fixed Eleventy output directory

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function(eleventyConfig) {
+  return {
+    dir: {
+      input: "src",  // Tells Eleventy to read from "src/"
+      output: "_site" // Tells Eleventy to put the final site in "_site/"
+    }
+  };
+};
+
+
+
